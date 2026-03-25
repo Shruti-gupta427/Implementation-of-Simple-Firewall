ef9319c1625b4dd25f988e7ae4bf7c485a03c9df
shrutiXcode <sg920450@gmail.com>
2026-03-25 18:58:01 +0530

    implemented api for our firewall

---
+++ backend/app.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+
+app = FastAPI(title="Firewall API")
+# cors configuration
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+blocked_ip = ["8.8.8.8"]
+
+@app.get("/get_list_blocked_ips")
+async def get_ips():
+    return {"blocked_ips": blocked_ip}
+
+@app.post("/block_ip")
+async def block_ip(request: Request):
+    data = await request.json()
+    ip = data.get("ip")
+    if not ip:
+        raise HTTPException(status_code=400, detail="No IP provided")
+    if ip not in blocked_ip:
+        blocked_ip.append(ip)
+        print(f"Added {ip} to list")
+        return {"status": "success", "blocked":ip}
+    return {"status": "error", "message": "Already in blocked ip list"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=5000)
--- backend/engine.py
+++ backend/engine.py
@@ -1,20 +1,31 @@
 import pydivert
+import requests
+import time
 blocked_ips = ["8.8.8.8", "10.128.10.240"]
-
+def get_blocked_ips():
+    try:
+        response=requests.get("http://127.0.0.1:5000/get_ips", timeout=0.2)
+        if response.status_code == 200:
+            return response.json().get("blocked_ip", [])
+    except Exception:
+        # if api is down
+        return ["8.8.8.8"]
+    return []
 def start_firewall_fun():
     print("Firewall is starting to work...",flush=True)
-    print(f"Currently blocking these guests: {blocked_ips}",flush=True)
     # KeyboardInterrupt
     print("   Press [Ctrl + C] on your keyboard to safely close the firewall and restore normal settings.")
     # Must run VS Code as Admin
     try:
-        with pydivert.WinDivert("ip") as network_tap:
+        with pydivert.WinDivert("true") as network_tap:
+            for packet in network_tap:
+                blocked_ip = get_blocked_ips()
             for packet in network_tap:
     
                 sender = packet.src_addr
                 receiver = packet.dst_addr
 
-                if sender in blocked_ips or receiver in blocked_ips:
+                if sender in blocked_ip or receiver in blocked_ip:
                     print(f"BLOCKED: {sender} tried to communicate with {receiver}",flush=True)
                     continue 
                 network_tap.send(packet)
