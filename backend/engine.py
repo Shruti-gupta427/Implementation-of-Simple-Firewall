b80758dd498aabd7badca5b16b6a13f75603ecfb
Krish1022sona <shivanshgupta1.india@gmail.com>
2026-03-29 10:30:36 +0530

    Fixed Bugs in engine.py

--- backend/engine.py
+++ backend/engine.py
@@ -4,9 +4,9 @@ import time
 blocked_ips = ["8.8.8.8", "10.128.10.240"]
 def get_blocked_ips():
     try:
-        response=requests.get("http://127.0.0.1:5000/get_ips", timeout=0.2)
+        response=requests.get("http://127.0.0.1:5000/get_list_blocked_ips", timeout=0.2) # ---fixed wrong url get_ips -> get_list_blocked_ips---
         if response.status_code == 200:
-            return response.json().get("blocked_ip", [])
+            return response.json().get("blocked_ips", []) # ---changed blocked_ip -> blocked_ips---
     except Exception:
         # if api is down
         return ["8.8.8.8"]
@@ -15,17 +15,26 @@ def start_firewall_fun():
     print("Firewall is starting to work...",flush=True)
     # KeyboardInterrupt
     print("   Press [Ctrl + C] on your keyboard to safely close the firewall and restore normal settings.")
+
+    blocked_ips_list = get_blocked_ips()
+    last_update_time = time.time()
+
     # Must run VS Code as Admin
     try:
         with pydivert.WinDivert("true") as network_tap:
             for packet in network_tap:
-                blocked_ip = get_blocked_ips()
-            for packet in network_tap:
+                # ---removed unneccessary loop "for packet in network_tap:"---
+
+                # ---Added new Code to check "Only check the API for new IPs every 5 seconds (Stops the internet crash!)"---
+                current_time = time.time()
+                if current_time - last_update_time > 5:
+                    blocked_ips_list = get_blocked_ips()
+                    last_update_time = current_time
     
                 sender = packet.src_addr
                 receiver = packet.dst_addr
 
-                if sender in blocked_ip or receiver in blocked_ip:
+                if sender in blocked_ips_list or receiver in blocked_ips_list:
                     print(f"BLOCKED: {sender} tried to communicate with {receiver}",flush=True)
                     continue 
                 network_tap.send(packet)
